a2ec80f08b7766d6015b2977005786cbcff7dadb
Avdhesh Goel <avdheshgoel@Avdheshs-MacBook-Pro-2.local>
2025-02-26 21:37:13 +0000

    E2e automation test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Change this to your application's base URL
+    baseUrl: 'https://react-808595275197.us-central1.run.app', // Change this to your application's base URL
     supportFile: 'cypress/support/index.js',
     fixturesFolder: 'cypress/fixtures',
     specPattern: 'cypress/integration/**/*.spec.js',
